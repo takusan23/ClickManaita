dc7469fac7b9aaee8a882db1fc6e254bcd9ea502
takusan23 <takumu1200@gmail.com>
2018-08-07 07:08:44 +0900

    Lang File

--- 1.12.2/main/java/com/takusan_23/clickmanaita/ClickManaitaCustom.java
+++ 1.12.2/main/java/com/takusan_23/clickmanaita/ClickManaitaCustom.java
@@ -42,15 +42,14 @@ public class ClickManaitaCustom extends Item {
         catch (NumberFormatException e){
             if (!worldIn.isRemote)
             {
-                player.sendMessage(new TextComponentString("--------------------"));
                 player.sendMessage(new TextComponentString("数字が正しくありません"));
                 player.sendMessage(new TextComponentString("金床で名前を増やしたい量の数字に変更してください。"));
-                player.sendMessage(new TextComponentString("上限はintの限界(2147483647)まで？整数のみ利用できます。"));
+                player.sendMessage(new TextComponentString("上限はintの限界まで？整数のみ利用できます。"));
                 player.sendMessage(new TextComponentString(""));
-                player.sendMessage(new TextComponentString("The number is incorrect"));
+                player.sendMessage(new TextComponentString(""));
+                player.sendMessage(new TextComponentString("The number is incorrect."));
                 player.sendMessage(new TextComponentString("Please change the number of numbers you want to increase the name on the anvil."));
-                player.sendMessage(new TextComponentString("Is the upper limit to the limit of int (2147483647)? Only integers are available."));
-                player.sendMessage(new TextComponentString("--------------------"));
+                player.sendMessage(new TextComponentString("Is the upper limit to the limit of int ? Only integers are available."));
             }
         }
 
